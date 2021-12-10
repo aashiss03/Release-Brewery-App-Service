6c448b18e9ca6127f5c76b6181a9c077e15d9b59
aashiss03@gmail.com <aashiss03@gmail.com>
2021-12-10 17:31:24 -0500

    initial commit

---
+++ src/main/java/com/springframework/ReleaseBreweryAppService/ReleaseBreweryAppServiceApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.ReleaseBreweryAppService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReleaseBreweryAppServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReleaseBreweryAppServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springframework/ReleaseBreweryAppService/ReleaseBreweryAppServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.ReleaseBreweryAppService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseBreweryAppServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
